c05875894d5cb9f730271647e2bef41c65067d7d
Ramrachai <ramrachaim@gmail.com>
2019-09-19 00:38:01 +0600

    Update: determine season and change text according to season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const SeasonDisplay = props => {
-  console.log(props.lat);
-  return <div> Season Display </div>;
+// determine season
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
 };
+const SeasonDisplay = props => {
+  let seasonText;
+  const season = getSeason(props.lat, new Date().getMonth());
+  if (season === "summer") {
+    seasonText = "Summer season , Sunny Day";
+  } else {
+    seasonText = "Winter Season, Cold day";
+  }
+  return <div>{seasonText}</div>;
+};
+
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   constructor(props) {
@@ -17,7 +18,7 @@ class App extends React.Component {
   }
   render() {
     if (this.state.lat && !this.state.errorMessage) {
-      return <div> Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
     if (!this.state.lat && this.state.errorMessage) {
       return <div> Error: {this.state.errorMessage}</div>;
