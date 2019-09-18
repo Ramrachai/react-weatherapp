464c2f0b54822f315c9c342e8d2d0b03059d3fc5
Ramrachai <ramrachaim@gmail.com>
2019-09-18 23:26:08 +0600

    update: user location determine

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+      errorMessage: ""
+    };
+  }
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
   }
   render() {
-    return <div>Hi </div>;
+    return (
+      <div>
+        {" "}
+        Lat: {this.state.lat} <br /> Error:{this.state.errorMessage}
+      </div>
+    );
   }
 }
 
